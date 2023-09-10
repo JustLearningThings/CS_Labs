87c8ed9d6a7f4dbb36445409e7c7944af15cbbd8
Smocvin Denis <denis.smocvin@isa.utm.md>
2023-09-10 20:25:29 +0300

    add lab1

--- Cipher.cs
+++ Cipher.cs
@@ -44,7 +44,7 @@ public class Cipher
         if (!_isValid(text))
             return "All characters must be between 'A' and 'Z', as well as between 'a' and 'z'!";
 
-        // dencryption
+        // decryption
         for (int i = 0; i < text.Length; i++)
         {
             result += _reversed_cryptogram[text[i]];
@@ -57,32 +57,62 @@ public class Cipher
     {
         if (shouldCreateKey2)
         {
-            char[] alphabet = new char[26];
+            char[] keys = new char[26];
+            char[] values = new char[26];
+            StringBuilder strBuilder = new();
+            HashSet<char> seen = new();
+            string duplicateFreeKey2 = "";
+            int k = 0;
+
+            foreach (char c in _key2)
+            {
+                if (!seen.Contains(c))
+                {
+                    duplicateFreeKey2 += Char.ToUpper(c);
+                    seen.Add(c);
+                }
+            }
+
+            seen = new();
 
             for (int i = 0; i < 26; i++)
-                alphabet[i] = (char)((int)'a' + i);
-
-            // remove duplicate letters in _key2
-            string duplicateFreeKey2 = _removeDuplicates();
-
-            // input the rest of the characters
-            for (char c = 'a'; c <= 'z'; c++)
             {
-                if (!_cryptogram.Keys.ToArray().Contains(c))
+                char letter = Char.ToUpper((char)((int)'a' + i));
+
+                _cryptogram[letter] = ' ';
+            }
+
+            keys = _cryptogram.Keys.ToArray();
+
+            // insert key in the cryptogram
+            for (int i = 0; i < duplicateFreeKey2.Length; i++)
+            {
+                _cryptogram[keys[i]] = duplicateFreeKey2[i];
+                seen.Add(duplicateFreeKey2[i]);
+
+                k = i;
+            }
+
+            k++;
+            
+            // insert the rest of the alphabet
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                if (!seen.Contains(c))
                 {
-                    _cryptogram[c] = ' ';
+                    _cryptogram[keys[k]] = c;
+                    seen.Add(c);
+
+                    k++;
                 }
             }
             
-            // create the reversed alphabet (permute the original alphabet)
-            char[] keys = _cryptogram.Keys.ToArray();
-            char[] permutedKeys = _cryptogram.Keys.ToArray();
-//            permutedKeys = Permute(permutedKeys, 0, permutedKeys.Length - 1);
+            // create the reverse cryptogram
+            values = _cryptogram.Values.ToArray();
 
-            for (int i = 0; i < _cryptogram.Count; i++)
+            for (int i = 0; i < values.Length; i++)
             {
-                _cryptogram[keys[i]] = permutedKeys[i];
-                _reversed_cryptogram[permutedKeys[i]] = keys[i];
+                _reversed_cryptogram[values[i]] = keys[i];
             }
         }
         else
@@ -108,43 +138,10 @@ public class Cipher
 
         return true;
     }
-
-    private string _removeDuplicates()
+    private char[]? Permute(char[] chars)
     {
-        StringBuilder strBuilder = new();
-        HashSet<char> seen = new();
-        string duplicateFreeKey2 = "";
 
-        foreach (char c in _key2)
-        {
-            if (!seen.Contains(c))
-            {
-                duplicateFreeKey2 += c;
-                seen.Add(c);
-            }
-        }
-        
-        // input the key first in the alphabet
-        for (int i = 0; i < duplicateFreeKey2.Length; i++)
-        {
-            _cryptogram[duplicateFreeKey2[i]] = ' ';
-        }
 
-        return duplicateFreeKey2;
-    }
-
-    private char[]? Permute(char[] chars, int l, int r)
-    {
-        if (l == r)
-            return chars;
-
-        for (int i = l; i <= r; i++)
-        {
-            (chars[l], chars[i]) = (chars[i], chars[l]);
-            Permute(chars, l + 1, r);
-            (chars[l], chars[i]) = (chars[i], chars[l]);
-        }
-
-        return null;
+        return chars;
     }
 }
